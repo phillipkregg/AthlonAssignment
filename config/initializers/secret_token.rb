5685dde381df79da5abb98c0db7bd67ba4532875
Your Name <phillipkregg@gmail.com>
2012-09-15 22:27:46 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AthlonAssignment::Application.config.secret_token = '75552e7378a3e4c9c08e98b3cf3ed4c4386dbbeb3dc2dddafb1cf6a8d0622b8fb161f6d1dd197a97ed942457697e436494dcbbd268183f4e2576698fa8950e33'
